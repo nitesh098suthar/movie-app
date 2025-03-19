41658ace5f3b380800a97e6872b9cb68efcb57b8
Nitesh Kumar <nitesh098suthar@gmail.com>
2025-03-19 17:19:38 +0530

    initial commit

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text className="text-primary text-5xl">prlimary</Text>
+    </View>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // NOTE: Update this to include the paths to all of your component files.
+  content: ["./app/**/*.{js,jsx,ts,tsx}"],
+  presets: [require("nativewind/preset")],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#030014",
+        secondary: "#151312",
+        light: {
+          100: "#D6C6FF",
+          100: "#A8B5DB",
+          100: "#9CA4AB",
+        },
+        dark: {
+          100: "#221f3d",
+          100: "#0f0d23",
+          100: "#",
+        },
+        accent: "#AB8BFF",
+      },
+    },
+  },
+  plugins: [],
+};
