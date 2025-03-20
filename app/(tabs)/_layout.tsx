2a66b33d7532da058313d3d7962297c086ee9686
Nitesh Kumar <nitesh098suthar@gmail.com>
2025-03-20 11:15:20 +0530

    icons and name added to the tabBarIcon

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Tabs } from "expo-router";
-
+import { Image, ImageBackground, Text } from "react-native";
 const _layout = () => {
   return (
     <Tabs>
@@ -9,6 +9,9 @@ const _layout = () => {
         options={{
           title: "home",
           headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon focused={focused} title={"Home"} />
+          ),
         }}
       />
       <Tabs.Screen
@@ -16,6 +19,9 @@ const _layout = () => {
         options={{
           title: "profile",
           headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon focused={focused} title={"Profile"} />
+          ),
         }}
       />
       <Tabs.Screen
@@ -23,6 +29,9 @@ const _layout = () => {
         options={{
           title: "saved",
           headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon focused={focused} title={"Saved"} />
+          ),
         }}
       />
       <Tabs.Screen
@@ -30,6 +39,9 @@ const _layout = () => {
         options={{
           title: "search",
           headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon focused={focused} title={"Search"} />
+          ),
         }}
       />
     </Tabs>
@@ -37,3 +49,29 @@ const _layout = () => {
 };
 
 export default _layout;
+
+const TabIcon = ({ focused, title }: any) => {
+  const iconMap: any = {
+    Home: require("@/assets/icons/home.png"),
+    Profile: require("@/assets/icons/person.png"),
+    Saved: require("@/assets/icons/save.png"),
+    Search: require("@/assets/icons/search.png"),
+  };
+
+  const iconSource: any = iconMap[title];
+
+  return (
+    <>
+      <ImageBackground
+        source={require("@/assets/images/highlight.png")}
+        className="flex-1 justify-center items-center min-h-14 rounded-full min-w-[112px] mt-4 overflow-hidden"
+        style={{ flexDirection: "row" }}
+      >
+        <Image source={iconSource} className="" />
+        <Text className="text-secondary text-base font-semibold ml-2">
+          {title}
+        </Text>
+      </ImageBackground>
+    </>
+  );
+};
