549cccc4c83727dcd7444ce028c4f0e7cc82e159
Nitesh Kumar <nitesh098suthar@gmail.com>
2025-03-20 10:26:51 +0530

    below tabs added and routing is also working

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { Tabs } from "expo-router";
+
+const _layout = () => {
+  return (
+    <Tabs>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "home",
+          headerShown: false,
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "profile",
+          headerShown: false,
+        }}
+      />
+      <Tabs.Screen
+        name="saved"
+        options={{
+          title: "saved",
+          headerShown: false,
+        }}
+      />
+      <Tabs.Screen
+        name="search"
+        options={{
+          title: "search",
+          headerShown: false,
+        }}
+      />
+    </Tabs>
+  );
+};
+
+export default _layout;
